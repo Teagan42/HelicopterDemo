af804468cbfb02af83ccd8e78f521455f99a7f66
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-05-25 22:50:11 -0600

    Add valid pin numbers

--- src/server/src/models/Pin.js
+++ src/server/src/models/Pin.js
@@ -129,7 +129,32 @@ var validInterupts = [
     , 'none'
 ];
 var validPinNumbers = [
-    20
+    2
+    , 3
+    , 4
+    , 17
+    , 27
+    , 22
+    , 10
+    , 9
+    , 11
+    , 5
+    , 6
+    , 13
+    , 19
+    , 26
+    , 14
+    , 15
+    , 18
+    , 23
+    , 24
+    , 25
+    , 8
+    , 7
+    , 12
+    , 16
+    , 20
+    , 21
 ];
 
 var validatePinId = function (pinId, res) {
