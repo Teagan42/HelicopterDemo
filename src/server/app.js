434a2608bb6e907832d14b25ba84ccedc4724f8c
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-05-25 21:52:02 -0600

    Application engine

---
+++ src/server/app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+require('./src/services/logger.js').init(require('./config.json'));
+
+var apiHandler = require('./src/handlers/api.js');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, './views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+apiHandler.setup(app);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    console.log('error ' + err);
+    res.status(err.status || 500);
+    res.send();
+    // res.render('error', {
+    //   message: err.message,
+    //   error: err
+    // });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error.html', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
