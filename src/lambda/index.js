428e7145c961de97952a42da0f4323b757c0dd1d
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-05-25 21:48:38 -0600

    Initial lambda code for alexa voice service.

---
+++ src/lambda/index.js
@@ -0,0 +1,231 @@
+var http = require('http');
+
+/**
+ * Route incoming request based on type
+ */
+exports.handler = function (event, context) {
+    console.log('Routing request');
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Session start event.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+}
+
+/**
+ * Skill launch without specification.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId +
+        ", sessionId=" + session.sessionId);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Skill launch with specification.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId +
+        ", sessionId=" + session.sessionId +
+        ", intentName=" + intentRequest.intent.name);
+    console.log(intentRequest.intent);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    switch (intentName) {
+        case "SetPinIntent":
+            setPinInSession(intent, session, callback);
+            break;
+        case "GetPinIntent":
+            getPinNumberFromSession(intent, session, callback);
+            break;
+        case "LaunchHelicopterIntent":
+            launchHelicopter(intent, session, callback);
+            break;
+        case "AMAZON.HelpIntent":
+            getWelcomeResponse(callback);
+            break;
+        case "AMAZON.StopIntent":
+        case "AMAZON.CancelIntent":
+            handleSessionEndRequest(callback);
+            break;
+        default:
+            throw "Invalid Intent";
+    }
+}
+
+/**
+ * On session ended event
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
+        ", sessionId=" + session.sessionId);
+    // Add cleanup logic here
+}
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var sessionAttributes = {};
+    var cardTitle = "Welcome";
+    var speechOutput = "Welcome to the launch pad. " +
+        "Initialize the launch pin before initiating launch.";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "Start by saying, set the launch pin to four";
+    var shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    var cardTitle = "Session Ended";
+    var speechOutput = "Bird is in the air!";
+    // Setting this to true ends the session and exits the skill.
+    var shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function setPinInSession(intent, session, callback) {
+    var pinNumberSlot = intent.slots.PinNumber;
+    var sessionAttributes = {};
+    var speechOutput = "";
+    var repromptText = "";
+    var shouldEndSession = false;
+    var cardTitle = "CARD TITLE";
+
+    if (pinNumberSlot) {
+        var pinNumber = pinNumberSlot.value;
+        sessionAttributes = createPinNumberAttributes(pinNumber);
+        speechOutput = "Pin " + pinNumber + " has been set for ignition.";
+        repromptText = "You can now launch the helicopter by saying, launch the helicopter";
+    } else {
+        speechOutput = "That is not a valid pin number. Please try again.";
+        repromptText = "That is not a valid pin number. You can set the pin number by saying, set the launch pin.";
+    }
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function createPinNumberAttributes(pinNumber) {
+    return {
+        pinNumber: pinNumber
+    };
+}
+
+function getPinNumberFromSession(intent, session, callback) {
+    var pinNumber;
+    var repromptText = null;
+    var sessionAttributes = {};
+    var shouldEndSession = false;
+    var speechOutput = "";
+
+    if (session.attributes) {
+        pinNumber = session.attributes.pinNumber;
+    }
+
+    if (pinNumber) {
+        speechOutput = "The launch pin is " + pinNumber + ".";
+    } else {
+        speechOutput = "I'm not sure what the launch pin is, you can say set launch pin 4.";
+    }
+
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+function launchHelicopter(intent, session, callback) {
+    var pinNumber;
+    var repromptText = "";
+    var speechOutput = "";
+    var shouldEndSession = true;
+
+    if (session.attributes) {
+        pinNumber = session.attributes.pinNumber;
+    }
+
+    if (pinNumber) {
+        // TODO : Call Endpoint
+        speechOutput = "Launch initiated!";
+        repromptText = null;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "Please set the launch pin before launching.";
+        repromptText = "Set the launch pin before launching by saying, set launch pin 4.";
+        shouldEndSession = false;
+    }
+
+    callback({},
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: "SessionSpeechlet - " + title,
+            content: "SessionSpeechlet - " + output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
